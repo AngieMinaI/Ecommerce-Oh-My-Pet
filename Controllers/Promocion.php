198602dcd16bf5d78217a2b23ad25f38785d06c9
asuna <asuna@gmail.com>
2024-08-01 01:47:53 -0500

    ecommerce

---
+++ Controllers/Promocion.php
@@ -0,0 +1,18 @@
+<?php
+
+class Promocion extends Controllers
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function promocion()
+    {
+        $data['page_tag'] = NOMBRE_EMPRESA;
+        $data['page_title'] = "Todos los productos";
+        $data['page_name'] = "Promociones";
+        $data['productos'] = $this->model->getPromocionProductos();
+        $this->views->getView($this, "promocion", $data);
+    }
+}
